062ef2c9995533f497591ca951807fc404e1fb73
Andrew Slejzer <brokensound548@gmail.com>
2019-03-05 14:06:20 -0500

    Made hash type required, added checking for files

--- hshu.py
+++ hshu.py
@@ -20,19 +20,22 @@ import argparse
 def main():
     # Argument parsing block, may move to its own module
     parser = argparse.ArgumentParser()
-    hsh_group = parser.add_mutually_exclusive_group()
     parser.add_argument("in_file", help="File to be hashed")
+    hsh_group = parser.add_mutually_exclusive_group(required=True)
     hsh_group.add_argument("--md5", help="flag to select md5 hash function", action="store_true")
     hsh_group.add_argument("--sha1", help="Flag to select sha1 hash function", action="store_true")
     hsh_group.add_argument("--sha256", help="flag to select sha256 hash function", action="store_true")
     parser.add_argument("--comp_file", '-c', help="file containing only a hash to be compared to the in_file")
+    parser.add_argument("--out_file", "-o", help="Flag to create a file containing the hash that was produced", action="store_true")
     args = parser.parse_args()
 
-    # print the file that the program has selected to run the hash on
-    print("running hash on", args.in_file)
-
     # open the selected files to read
-    afile = open(args.in_file, 'rb')
+    if args.in_file is not None:
+        afile = open(args.in_file, 'rb')
+        print("running hash on", args.in_file)
+    else:
+        print("No file selected")
+
     if args.comp_file is not None:
         bfile = open(args.comp_file, 'r')
         print(bfile.read())
@@ -40,6 +43,7 @@ def main():
     # if block to call the selected hash function
     if args.md5:
         print(hsh_funct.md5_hsh(afile), args.in_file)
+        print(args.md5)
     elif args.sha1:
         print(hsh_funct.sha1_hsh(afile), args.in_file)
     elif args.sha256:
@@ -47,8 +51,11 @@ def main():
     else:
         print("No hash function specified, nothing generated")
 
-    # close the file that was hashed
-    afile.close()
+    # close the files that were opened
+    if args.in_file is not None:
+        afile.close()
+    if args.comp_file is not None:
+        bfile.close()
 
 
 if __name__ == '__main__':
