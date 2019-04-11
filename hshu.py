53c7df9bfefb56fb3f37c300e766f2243e84094f
BeefKastle <brokensound548@gmail.com>
2019-04-11 14:52:45 -0400

    moved final program output to independent functions

--- hshu.py
+++ hshu.py
@@ -16,6 +16,27 @@ import argparse
 import hsh_digest
 
 
+def output_to_file(write_digest):
+    try:
+        out_file = open("%s.digest" % write_digest.file_name, "w+")
+        out_file.write("%s ---------------- %s\n" % (write_digest.digest, write_digest.file_name))
+        out_file.close()
+        pass
+    except:
+        print("An error occurred in creating the outfile")
+
+
+def append_to_file(append_digest, append_file):
+    try:
+        app_file = open(append_file, 'a')
+        app_file.write("%s ---------------- %s\n" % (append_digest.digest, append_digest.file_name))
+        app_file.close()
+    except:
+        print("Error opening append file")
+
+def output_to_console(out_digest):
+    print("%s ---------------- %s" % (out_digest.digest, out_digest.file_name))
+
 def compare_hash_file(comp_digest, comp_file):
     try:
         file = open(comp_file, 'r')
@@ -43,13 +64,13 @@ def main():
     # Argument parsing block
     parser = argparse.ArgumentParser()
     parser.add_argument("--in_file", "-i", help="File to be hashed")
+    parser.add_argument("--hash_type", '-t', help="specify hash type to be used here, supports \"md5\", \"sha1\", and \"sha256\"")
     comp_group = parser.add_mutually_exclusive_group()
     file_group = parser.add_mutually_exclusive_group()
-    parser.add_argument("--hash_type", '-t', help="specify hash type to be used here, supports \"md5\", \"sha1\", and \"sha256\"")
     comp_group.add_argument("--comp_file", '-c', help="selects file hash digests to be compared to the digest of in_file")
     comp_group.add_argument("--comp_string", '-s', help="takes a string as an argument and compares the hash generated to it.")
     file_group.add_argument("--out_file", "-o", help="Flag to create a file containing the hash that was produced",
-                        action="store_true")
+                            action="store_true")
     file_group.add_argument("--append_file", '-a', help="Selects a file to append the hash output to")
 
     args = parser.parse_args()  # object that contains all of the arguments passed to the program
@@ -58,6 +79,7 @@ def main():
 
     # Create an object to store the file, file name, hash type and actual digest
     digest_store = hsh_digest.hsh_digest()
+
 
 
     # make sure to set the file that is going to be hashed
@@ -89,23 +111,13 @@ def main():
 
     # check if user has specified that they want the output of the hash written to a file
     if args.out_file:
-        try:
-            out_file = open("%s.digest" % digest_store.file_name, "w+")
-            out_file.write("%s ---------------- %s\n" % (digest_store.digest, digest_store.file_name))
-            out_file.close()
-
-        except:
-            print("An error occurred in creating the outfile")
+        output_to_file(digest_store)
 
     elif args.append_file is not None:
-        try:
-            app_file = open(args.append_file, 'a')
-            app_file.write("%s ---------------- %s\n" % (digest_store.digest, digest_store.file_name))
-        except:
-            print("Error opening append file")
+        append_to_file(digest_store, args.append_file)
 
     else:
-        print("%s ---------------- %s" % (digest_store.digest, digest_store.file_name))
+        output_to_console(digest_store)
 
 
 
